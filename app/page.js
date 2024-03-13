6854035c335f5196c4a4d845208c74c4fb8a9ee9
Rdebora <riosdeborasabrina@gmail.com>
2024-03-13 14:13:56 -0300

    add dark-mode

--- app/page.js
+++ app/page.js
@@ -1,15 +1,18 @@
-import { Nav } from "@/Components/nav";
-import styles from "./page.module.css";
-import { Contact } from "@/Components/contact";
+"use client";
 
-export default function Home() {
+import styles from "./page.module.css";
+
+import { NextUIProvider } from "@nextui-org/react"; // Install and correct path
+
+export default function Home({ children }) {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>Portafolio</p>
-        <Nav></Nav>
-        <Contact></Contact>
-      </div>
-    </main>
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        {children}
+        <main className={styles.main}>
+          <div className={styles.description}></div>
+        </main>
+      </NextThemesProvider>
+    </NextUIProvider>
   );
 }
